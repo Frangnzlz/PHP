265278f777b15deddd9e0418b7cef4f420753fba
MEDAC_T <francisco10david10@gmail.com>
2025-03-07 19:16:53 +0100

    get e inicio post

--- API/tabla1.php
+++ API/tabla1.php
@@ -1,16 +1,69 @@
 <?php
-    function bocataLomo(){
-        $datos = [
-            "bocata" => "ya",
-            "albaricoque" => "nose",
-            "melocoton" => "Almibar"
-        ];
+    require "../conexion.php";
 
-        return json_encode($datos);
+    $method = $_GET["method"];
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+
+    switch ($method){
+        case "GET":
+            echo casoGET($_conexion);
+            break;
+        case "POST":
+            echo casoPOST($_conexion);
+            break;
     }
 
-    echo bocataLomo();
 
 
+    function casoGET($_conexion){
+        if(isset($_GET["id_juego"])){
+            $consulta = "SELECT * FROM Juegos WHERE id_juego = :id";
+            $stmt = $_conexion -> prepare($consulta);
+            $stmt -> execute([
+                "id" => $_GET["id_juego"]
+            ]);
+            if($stmt -> rowCount() == 0){
+                return json_encode([["Error" => "No has introducido un valor valido"]]);
+            }else{
+                $res = $stmt -> fetchAll(pdo::FETCH_ASSOC);
+                return json_encode($res);
+            }
+        }else{
+            $consulta = "SELECT * FROM juegos";
+            $stmt = $_conexion -> prepare($consulta);
+            $stmt -> execute();
+            $res = $stmt -> fetchAll(pdo::FETCH_ASSOC);
+            return json_encode($res);            
+        }
+    }
 
+    function casoPOST($_conexion){
+        if(!isset($_GET["nombre"]) && !isset($_GET["genero"]) && !isset($_GET["fecha_lanzamiento"]) && !isset($_GET["id_desarrollador"])){
+            $result = ["ERROR" =>  "Faltan campos por rellenar"];
+        }else{
+            $nombre = $_GET["nombre"];
+            $genero = $_GET["genero"];
+            $fecha = $_GET["fecha_lanzamiento"];
+            $id_desarrollador = $_GET["id_desarrollador"];
+            $consulta = "INSERT INTO juegos 
+            (nombre, genero, fecha_lanzamiento, id_desarrollador) VALUE
+            :n, :g, :f, :d";
+            $stmt = $_conexion -> prepare($consulta);
+            $introducido = $stmt -> execute([
+                "n" => $nombre,
+                "g" => $genero, 
+                "f" => $fecha,
+                "d" => $id_desarrollador
+            ]);
+            if($introducido){
+                $result = ["EXITO" =>  "Juego introducido"];
+            }else{
+                $result = ["ERROR" =>  "Algun campo no es correcto"];
+            }
+
+        }
+        return json_encode($result);
+    }
 ?>
--- api_tabla1.php
+++ api_tabla1.php
@@ -23,17 +23,61 @@
         ini_set("display_errors", 1);
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             $method = $_POST["method"];
-            $apiURL ="http://localhost/PHP/API/tabla1.php";
-
-            $curl = curl_init();
+            $apiURL ="http://localhost/PHP/API/tabla1.php?method=$method";
     
-            curl_setopt($curl, CURLOPT_URL, $apiURL);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $res = curl_exec($curl);
-            curl_close($curl);
-            $datos = json_decode(json: $res);
-            foreach($datos as $campo => $nose){
-                echo "$campo => $nose";
+            if($method == "GET"){
+                if($_POST["id_juego"] != ""){
+                    $id = "&id_juego=$_POST[id_juego]";
+
+                    $apiURL .= $id;
+                }
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $apiURL);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                $res = curl_exec($curl);
+                curl_close($curl);
+                $datos = json_decode($res, true);
+
+                ?>
+                <table border="1">
+                    <tr>
+                        <?php
+                            foreach($datos[0] as $campo => $valor){?>
+                                <th><?= $campo ?></th>
+                        <?php };
+                        ?>
+                    </tr>
+                    <?php
+                        for($i = 0; $i < count($datos); $i++ ){
+                            ?><tr><?php
+                            foreach($datos[$i] as $campo => $valor){?>
+                                <td><?= $valor ?></td>
+                        <?php };
+                        ?>
+                        </tr>    
+                    <?php }
+                        ?>
+                    </tr>
+                </table>
+                <?php
+            }else if($method == "POST"){
+                $nombre = $_POST["nombre"];
+                $genero = $_POST["genero"];
+                $fecha = $_POST["fecha_lanzamiento"];
+                $id_desarrollador = $_POST["id_desarrollador"];
+                $apiURL .=  "&nombre=$nombre&genero=$genero&fecha_lanzamiento=$fecha&id_desarrollador=$id_desarrollador";
+
+                echo $apiURL;
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $apiURL);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                $res = curl_exec($curl);
+                curl_close($curl);
+                $datos = json_decode($res, true);
+                var_dump($datos);
+                foreach($datos as $valor => $texto){
+                    echo "<h3>$valor : $text </h3>";
+                }
             }
         }
     ?>
